165b5c3026e6ea105e3782ca2f7068938a2d1a20
Aditipanesar <102141157+Aditipanesar@users.noreply.github.com>
2024-07-24 20:22:08 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        TicTacToe tictactoe =new TicTacToe();   //creating_interface
+        TicTacToe tictactoe =new TicTacToe();   
 
 
     }
